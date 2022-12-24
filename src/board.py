114b1cf2b0db333b1e086b4184b2973b804158eb
hugomal <hugomal@kth.se>
2022-12-24 10:58:12 +0100

    added stuff

--- src/board.py
+++ src/board.py
@@ -3,13 +3,21 @@
 class Board():
     def __init__(self):
         self.slots = [[]]
-        self.layout = None
-
-        self.set_layout()
-    
-    def set_layout(self):
-        """Sets board layout to Fox and Hen game"""
         self.layout = [3, 3, 7, 7, 7, 3, 3]
+        self.neighbor_matrix = [[[ [] ] for _ in range(max(self.layout))] for _ in range(len(self.layout))]
+
+        self.set_neighbor_matrix()
+
+    def set_neighbor_matrix(self):
+        for i in range(len(self.layout)):
+            for j in range(self.layout[i]):
+                # Set right/left neighbors
+                if j != 0:
+                    self.neighbor_matrix[i][j].append((i,j-1))
+                if j != self.layout[i]-1:
+                    self.neighbor_matrix[i][j].append((i,j+1))
+                
+        pass
     
     def set_slot(self, row, col, piece):
         self.slots[row][col] = piece
--- src/window.py
+++ src/window.py
@@ -12,14 +12,30 @@ class Window():
 
     def draw_board(self, board):
         """Draw layout"""
+        # List with lists of all xpos and list with all ypos
+        xpos_list, ypos_list = [],[]
         ypos = self.height / 2 - math.floor(len(board.layout) / 2) * self.slot_spacing
         for slots in board.layout:
+            current_xpos_list = []
             xpos = self.width / 2 - math.floor(slots / 2) * self.slot_spacing
             for i in range(slots):
                 pygame.draw.circle(self.win, (0, 0, 0), (xpos, ypos), 5)
+                current_xpos_list.append(xpos)
                 xpos += self.slot_spacing
+            xpos_list.append(current_xpos_list)
+            ypos_list.append(ypos)
             ypos += self.slot_spacing
-                
+        """Draw lines"""
+        for i in range(len(ypos_list)):
+            pygame.draw.line(self.win, (0,0,0), (xpos_list[i][0],ypos_list[i]), (xpos_list[i][-1],ypos_list[i]), width=3)
+            if i == len(ypos_list)-1:
+                break
+            for xpos in xpos_list[i]:
+                if xpos in xpos_list[i+1]:
+                    pygame.draw.line(self.win, (255,0,0), (xpos, ypos_list[i]), (xpos, ypos_list[i+1]), width=3)
+        
+        pygame.draw.line(self.win, (0,255,0), (xpos_list[0][0], ypos_list[0]), (xpos_list[2][4], ypos_list[2]), width=3)
+        
 
         """Draw Pieces"""
         pass
